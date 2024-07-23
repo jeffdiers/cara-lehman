ec06bc28974484caf7f5060b2c20ebcbc2448c79
Jeff Diers <jeff.diers@gmail.com>
2024-07-23 10:40:37 -0400

    fake worker

--- components/pdf-viewer.tsx
+++ components/pdf-viewer.tsx
@@ -6,10 +6,10 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export function CustomPDFViewer({ pdfFile }: { pdfFile: string }) {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-      <div className="h-full w-full">
-        <Viewer fileUrl={pdfFile} />
-      </div>
-    </Worker>
+    // <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+    <div className="h-full w-full">
+      <Viewer fileUrl={pdfFile} />
+    </div>
+    // </Worker>
   );
 }
