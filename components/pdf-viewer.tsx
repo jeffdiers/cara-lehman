5c53d9490102f993c84be7768039c9a46b203c65
Jeff Diers <jeff.diers@gmail.com>
2024-07-22 22:04:04 -0400

    toml

--- components/pdf-viewer.tsx
+++ components/pdf-viewer.tsx
@@ -1,13 +1,14 @@
-import { Viewer, Worker } from "@react-pdf-viewer/core";
+// import { Viewer, Worker } from "@react-pdf-viewer/core";
 
-import "@react-pdf-viewer/core/lib/styles/index.css";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export function CustomPDFViewer({ pdfFile }: { pdfFile: string }) {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-      <div className="h-full w-full">
-        <Viewer fileUrl={pdfFile} />
-      </div>
-    </Worker>
+    <div>no</div>
+    // <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+    //   <div className="h-full w-full">
+    //     <Viewer fileUrl={pdfFile} />
+    //   </div>
+    // </Worker>
   );
 }
